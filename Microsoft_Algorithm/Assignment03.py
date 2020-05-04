8e9beee5ce6aedbf3ab2e017a9ad19046f62b64a
lingchen2 <289140347@qq.com>
2020-05-04 13:37:07 +0800

    update acc

--- Microsoft_Algorithm/Assignment03.py
+++ Microsoft_Algorithm/Assignment03.py
@@ -13,22 +13,30 @@ print('over-fitting')
 
 # 3.1 In the first session of the practical part, you will implement an image classification model using any deep learning libraries that you are familiar with, which means, except for tensorflow and keras, you can also use pytorch/caffe/... . The dataset used in this session is the cifar10 which contains 50000 color (RGB) images, each with size 32x32x3. All 50000 images are classified into ten categories.
 
-from keras import models, layers, optimizers, datasets, utils
+from keras import models, layers, optimizers, datasets, utils, regularizers
 import matplotlib.pyplot as plt
 import numpy as np
 
 (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
 print(x_test.shape)
-
+# for i in range(1, 11):
+#       plt.subplot(2, 5, i)
+#       plt.imshow(x_train[i - 1])
+#       plt.text(3, 10, str(y_train[i-1]))
+#       plt.xticks([])
+#       plt.yticks([])
+# plt.show()
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2), strides=2))
+model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2), strides=2))
+model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2), strides=2))
 model.add(layers.Flatten())
-model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(10, activation='softmax'))
 model.summary()
@@ -37,24 +45,17 @@ x_train = x_train/255
 x_test = x_test/255
 y_train = utils.to_categorical(y_train, 10)
 y_test = utils.to_categorical(y_test, 10)
-model.fit(x_train, y_train, batch_size=200, epochs=100)
-# modelname = 'cifarfitmodel.h5'
-# model.save(modelname)
-# model = models.load_model('cifarfitmodel.h5')
+model.fit(x_train, y_train, batch_size=200, epochs=50)
+modelname = 'cifarfitmodel.h5'
+model.save(modelname)
+model = models.load_model('cifarfitmodel.h5')
 scores = model.evaluate(x_test, y_test)
 print(scores)
 result = model.predict_classes(x_test[:50])
 print(result)
-# print('-------')
-# print(y_test[:50])
-# print(np.argmax(result, axis=1))
 
-for i in range(1, 11):
-      plt.subplot(2, 5, i)
-      plt.imshow(x_train[i - 1])
-      plt.text(3, 10, str(y_train[i-1]))
-      plt.xticks([])
-      plt.yticks([])
-plt.show()
+# score = [1.1631775154471398, 0.8282999992370605]
+
+
 
 
